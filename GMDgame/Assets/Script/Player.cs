7c0e29978c677c499684fedb541cb529963edede
hhes0225 <hhes0225@gmail.com>
2023-04-13 16:07:18 +0200

    player HP bar UI
    - player HP bar UI(increase/decrease)
    - implement some player state(damaged, recovered, death)
    - create Character abstract class
    - create Player class(management?)

---
+++ GMDgame/Assets/Script/Player.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    bool isHurt { get; set; } = false;
+
+    [SerializeField]
+    private HPbarController hpBar;
+    
+    void Start()
+    {
+        //maxHP = 100;
+
+        //Debug.Log(maxHP);
+        hpBar.Initalize(maxHP, maxHP);
+        Debug.Log(hpBar.thisCurrentValue);
+        Debug.Log(hpBar.thisMaxValue);
+    }
+
+    void Update()
+    {
+        //testing health up/down function
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            PlayerDamaged();
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            PlayerRecovered();
+        }
+
+        //check game over state
+        if (hpBar.thisCurrentValue == 0)
+        {
+            PlayerDead();
+        }
+    }
+
+    //player not dead, just damaged
+    public void PlayerDamaged()
+    {
+        //if (isHurt)
+        //    return;
+        //isHurt = true;
+
+        Debug.Log("Player Damaged");
+        hpBar.thisCurrentValue -= 10;
+
+        //some damaged effect(animation or coroutine)
+        //for example, knock-back
+    }
+
+    public void PlayerRecovered()
+    {
+        Debug.Log("Player Recovered");
+        hpBar.thisCurrentValue += 10;
+    }
+
+    //player dead(hp is under 0)
+    public void PlayerDead()
+    {
+        Debug.Log("player dead");
+        //load gameover scene
+    }
+}
