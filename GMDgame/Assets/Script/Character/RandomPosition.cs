1350cb42fae331180eef98f1fb997d2a0aea15ba
elisemarque <elise.marque@etu.unistra.fr>
2023-05-06 07:57:14 +0200

    [Unity + script] add random spawn spot

--- GMDgame/Assets/Script/Character/RandomPosition.cs
+++ GMDgame/Assets/Script/Character/RandomPosition.cs
@@ -36,19 +36,7 @@ namespace Script.Character
                 return;
             } while ( true && safetyCount++ < safetyLimit );
             Debug.LogWarning ( $"Could not spawn player after {safetyLimit} attempts. Are your ray start values correct?" );
-            
-            /*RaycastHit hit;
 
-            Vector3 randomPosition = new Vector3(Random.Range(0, 1001), 164, Random.Range(0, 1001));
-            if (Physics.Raycast(randomPosition + Vector3.up, Vector3.down, out hit, 2000.0f))
-            {
-                Instantiate(player, hit.point, Quaternion.identity);
-            }
-            else
-            {
-                Debug.Log("there seems to be no ground at this position");
-            }*/
-            
         }
     }
 }
